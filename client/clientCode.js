2a6688e13cd74e8802e22622d04a8ed70e4415e6
Eli Gukovsky <gukovskye@gmail.com>
2016-05-22 05:13:10 +0000

    Start working on spotify integration

--- client/clientCode.js
+++ client/clientCode.js
@@ -12,3 +12,14 @@ Template.home.events({
 		});
 	}
 });
+
+Template.home.onRendered(function(){
+	Meteor.call("listGenres", function(err, data) {
+		if (err) {
+			console.log(err);
+		}
+		else {
+			console.log(data);
+		}
+	});
+});
--- server/methods.js
+++ server/methods.js
@@ -1,3 +1,12 @@
 Meteor.methods({
-
+	"listGenres": function() {
+		this.unblock();
+		try {
+			// TODO: header
+			var result = HTTP.get("https://api.spotify.com/v1/recommendations/available-genre-seeds");
+			return true;
+		} catch (e) {
+			return false;
+		}
+	}
 });
