9a516a18897d9d53bd24ed009599ce7e56c025ce
DarkAce65 <tvdood99@gmail.com>
2016-05-22 09:26:09 -0400

    Build UI

--- client/clientCode.js
+++ client/clientCode.js
@@ -1,18 +1,3 @@
-Template.home.helpers({
-	"spotifySrc": function() {
-		return Session.get("spotifySrc");
-	}
-});
-
-Template.home.events({
-	"click #searchTracks": function() {
-		$.get("https://api.spotify.com/v1/search", {"q": "Muse", "type": "track"}, function(data) {
-			var first = data.tracks.items[0].uri;
-			Session.set("spotifySrc", "https://embed.spotify.com/?uri=" + first);
-		});
-	}
-});
-
 Template.home.onRendered(function(){
 	Meteor.call("getAccessToken", function(err, data) {
 		if (err) {
@@ -21,6 +6,37 @@ Template.home.onRendered(function(){
 		else {
 			console.log(data);
 			Session.set("accessToken", data);
+			Meteor.call("listGenres", data, function(err, data) {
+				if (err) {
+					console.log(err);
+				}
+				else {
+					console.log(data);
+					Session.set("genres", data.data.genres);
+				}
+			});
 		}
 	});
+});
+
+Template.home.helpers({
+	"genres": function() {
+		return Session.get("genres");
+	},
+	"spotifySrc": function() {
+		return Session.get("spotifySrc");
+	}
+});
+
+Template.home.events({
+	"submit form": function(e) {
+		e.preventDefault();
+		console.log(e);
+	},
+	"click #searchTracks": function() {
+		$.get("https://api.spotify.com/v1/search", {"q": "Muse", "type": "track"}, function(data) {
+			var first = data.tracks.items[0].uri;
+			Session.set("spotifySrc", "https://embed.spotify.com/?uri=" + first);
+		});
+	}
 });
